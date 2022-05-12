fd9e9ad43cf2f91e547ce6a5e23c1ebc62a5b1d2
Dsfr62 <saviodudu79@gmail.com>
2022-05-12 16:45:16 -0300

    events

---
+++ exercicios-e-desafios/events.js
@@ -0,0 +1,19 @@
+function clickMe () {
+    console.log("Yeah, nice test");
+};
+
+function focusMe () {
+    console.log("Focus time");
+};
+
+function blurMe () {
+    console.log("Oops, no focus.");
+};
+
+function changeMe () {
+    console.log("Changed.")
+};
+
+function overMe () {
+    console.log("Good boy!")
+}
